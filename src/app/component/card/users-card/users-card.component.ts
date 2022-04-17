e7694937d8d3bfb1db7cc11446d76c17cb4e0240
Mert Saraç <mertsarac@yandex.com>
2022-04-17 19:29:19 +0300

    fixed sonar issues

--- src/app/component/card/users-card/users-card.component.ts
+++ src/app/component/card/users-card/users-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { APIGetUsersMetaModel, APIGetUsersUserResponseModel } from 'src/app/APIModels/APIResponseModels/APIGetUsersResponseModel';
 import { AuthenticationService } from 'src/app/service/auth/authentication.service';
 
--- src/app/page/dashboard-layout/users-page/users-page.component.ts
+++ src/app/page/dashboard-layout/users-page/users-page.component.ts
@@ -1,20 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { APIGetUsersMetaModel, APIGetUsersUserResponseModel } from 'src/app/APIModels/APIResponseModels/APIGetUsersResponseModel';
-import { AuthenticationService } from 'src/app/service/auth/authentication.service';
 
 @Component({
   selector: 'app-users-page',
   templateUrl: './users-page.component.html',
   styleUrls: ['./users-page.component.scss']
 })
-export class UsersPageComponent implements OnInit {
+export class UsersPageComponent {
   users: APIGetUsersUserResponseModel[];
   meta: APIGetUsersMetaModel;
 
-  constructor(
-  ) { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() { /* intentionally blank */ }
 }
