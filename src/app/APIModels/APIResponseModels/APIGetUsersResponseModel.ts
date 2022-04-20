fdabf83d2f12bca4b1b1d3efda1c3700d0104014
Furkan Yavuz <furknyavuz@gmail.com>
2022-04-20 11:25:17 +0300

    replace hard coded brand info with brand.data

--- src/app/APIModels/APIResponseModels/APIGetUsersResponseModel.ts
+++ src/app/APIModels/APIResponseModels/APIGetUsersResponseModel.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@angular/core';
+import { ResponseBaseAdapter } from '../../adapter/response-base.adapter';
 
 export interface APIGetUsersResponseModel {
-    users: APIGetUsersUserResponseModel[]
-    meta: APIGetUsersMetaModel
+  users: APIGetUsersUserResponseModel[];
+  meta: APIGetUsersMetaModel;
 }
 
 export interface APIGetUsersUserResponseModel {
-    username: string
+  username: string;
 }
 
 export interface APIGetUsersMetaModel {
-    offset: number
-    limit: number
-    count: number
-    countUseless: number
+  offset: number;
+  limit: number;
+  count: number;
+  countUseless: number;
 }
 
-@Injectable({
-    providedIn: "root",
-  })
+@Injectable( {
+  providedIn: 'root',
+} )
 export class APIGetUsersAdapter implements ResponseBaseAdapter<APIGetUsersResponseModel> {
-    adapt(item: any): APIGetUsersResponseModel {
-        return item as APIGetUsersResponseModel
-    }
+  adapt( item: any ): APIGetUsersResponseModel {
+    return item as APIGetUsersResponseModel;
+  }
 }
